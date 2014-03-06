047eaf9da49455014365612aebbed1812f38d1b7
chadcolby <chad.david.colby@gmail.com>
2014-03-06 09:25:49 -0800

    Setting up board branch

---
+++ NSCards/GameBoardViewController.m
@@ -0,0 +1,38 @@
+//
+//  GameBoardViewController.m
+//  NSCards
+//
+//  Created by Chad D Colby on 3/6/14.
+//  Copyright (c) 2014 Andrew Rodgers. All rights reserved.
+//
+
+#import "GameBoardViewController.h"
+
+@interface GameBoardViewController ()
+
+@end
+
+@implementation GameBoardViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
